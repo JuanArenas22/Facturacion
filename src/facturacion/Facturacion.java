3d799e65872a1680c0c92e698b4a0814c2690012
MichaelSmm98 <50148082+MichaelSmm98@users.noreply.github.com>
2020-03-31 19:37:48 -0500

    Hola Juan
    segunda

--- src/facturacion/Facturacion.java
+++ src/facturacion/Facturacion.java
@@ -16,6 +16,8 @@ public class Facturacion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hola Juan");
     }
     
 }
